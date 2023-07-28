cf84377c806bdf4afe119ec6b122078925a4825d
adonisjr <adonisjr.suico@gmail.com>
2023-07-28 12:05:00 +0800

    added social media links and modified background color

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,7 +11,7 @@ export default function Navbar() {
   const scrollPosition = useScrollPosition();
   console.log(scrollPosition)
   return (
-    <nav className={classNames(scrollPosition > 0 ? 'bg-white text-black' : 'shadow-none text-white',
+    <nav className={classNames(scrollPosition > 0 ? 'bg-white text-black' : 'shadow-none bg-transparent',
      'flex flex-col justify-between items-center transition sticky top-0 z-100 duration-1000 sm:pl-24 sm:flex-row sm:pr-12 sm:h-24')}>
       <div className='font-mono text-blue-600 text-xl'>
         Adonis
